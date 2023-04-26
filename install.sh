84139b9f6687d108f389cc5809204be564d7ca44
Andrew Druffner <andrew@nomstock.com>
2023-04-25 18:01:01 -0700

    fix: use absolute path

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ ln -s $HOME/bin/dotts.sh $HOME/bin/dotts
 git init --bare $HOME/.dotts
 
 mkdir -p .dotts-backup
-dotts checkout  -b master
+$HOME/bin/dotts checkout  -b master
 if [ $? = 0 ]; then
   echo "Checked out dotts.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-dotts checkout -b master
-dotts config status.showUntrackedFiles no
+$HOME/bin/dotts checkout -b master
+$HOME/bin/dotts config status.showUntrackedFiles no
 
 echo '.dotts' > $HOME/.gitignore
     
-dotts add  $HOME/.gitignore
-dotts commit -m 'initial commit'
+$HOME/bin/dotts add  $HOME/.gitignore
+$HOME/bin/dotts commit -m 'initial commit'
 
 
